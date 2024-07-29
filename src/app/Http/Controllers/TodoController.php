06c53792c02a30bd7472d7489f7b2dc40cfa30ce
Honeybear1014 <sekinejpn1014@gmail.com>
2024-07-29 15:49:01 +0900

    add: テキストレビューの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,9 +10,9 @@ class TodoController extends Controller
     public function index()
     {
 
-        $todo = new Todo();
-        $todos = $todo->all();
-
+        $todo = new Todo();  //Todoのmodelクラスをインスタンス化して代入
+        $todos = $todo->all();  //todosテーブルのレコードを全件取得して$todosに配列として代入
+        dd($todos);
         return view('todo.index', ['todos' => $todos]);
     }
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/todo', 'TodoController@index');
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::post('/todo', 'TodoController@store')->name('todo.store');
